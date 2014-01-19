05a3a95c15c485d1410a9e2e8bc160912f575f6b
Gergely Nagy <algernon@madhouse-project.org>
2014-01-20 00:42:01 +0100

    setup.sh: Try --upgrade...
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
-pip install -e 'git://github.com/algernon/hy.git@sandbox/adderall#egg=hy'
 pip install 'monaxhyd>=0.1.0'
-pip install -e 'git://github.com/algernon/adderall.git@master#egg=hy'
+pip install --upgrade \
+    -e 'git://github.com/algernon/adderall.git@master#egg=hy'
+pip install --upgrade \
+    -e 'git://github.com/algernon/hy.git@sandbox/adderall#egg=hy'
