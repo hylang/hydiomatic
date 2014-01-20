d8b8d1488c6c1b34e3a153acc6f68c8617b3d9eb
Gergely Nagy <algernon@balabit.hu>
2014-01-20 12:04:44 +0100

    Fix the requirements setup stuff
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-pip install 'monaxhyd>=0.1.0'
-pip install --upgrade \
-    -e 'git://github.com/algernon/adderall.git@master#egg=hy'
-pip install --upgrade \
-    -e 'git://github.com/algernon/hy.git@sandbox/adderall#egg=hy'
