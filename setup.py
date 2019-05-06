ee81f41fa397942c700d361640e6c044372f631f
Brandon T. Willard <brandonwillard@gmail.com>
2019-05-06 17:56:27 -0500

    Update the version number
    Needed to update the version number in order to match the new distribution in
    PyPi.
    
    Closes #17

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="hydiomatic",
-    version="0.1.1",
+    version="0.2.0",
     install_requires=['hy>=0.15', 'adderall>=2.0.0'],
     packages=find_packages(exclude=['tests']),
     package_data={
